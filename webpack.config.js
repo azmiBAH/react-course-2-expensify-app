149866919a3ddc51500294e07e31638432d7f01a
azmi <hattaazmi@yahoo.com>
2019-04-23 19:34:45 +0800

    tring to resolve heroku push failure

--- webpack.config.js
+++ webpack.config.js
@@ -43,7 +43,7 @@ module.exports = (env) => {
         devServer: {
             contentBase: path.join(__dirname, 'public'),
             historyApiFallback: true
-        }
-
+        },
+        NPM_CONFIG_PRODUCTION: false
     };
 }
